1706b073a18d8a7ddf6e2f4521afc174e7c59188
JustDondo <162633910+CodeDondo@users.noreply.github.com>
2025-03-17 10:11:33 +0100

    addedImageModel

---
+++ models/imageModel.js
@@ -0,0 +1,32 @@
+import dbConfig from "../config/dbConfig";
+import { DataTypes, Model } from "sequelize";
+
+export class imageModel extends Model { }
+
+imageModel.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    filename: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    author: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    }
+},
+    {
+        sequelize: dbConfig,
+        modelName: "image",
+        timestamps: true,
+        underscored: true,
+    }
+);
