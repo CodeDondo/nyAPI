866281b0ec641fc5c8ef95b01273c69128d8f9be
JustDondo <162633910+CodeDondo@users.noreply.github.com>
2025-03-19 11:12:21 +0100

    fixed

--- utils/apiUtils.js
+++ utils/apiUtils.js
@@ -1,6 +1,8 @@
+/**
 @param {Object}
 @param {String}
 @returns {Array}
+*/
 
 export const getQueryAttributes = (query, default_attr) => {
     const { attributes } = query
@@ -8,16 +10,20 @@ export const getQueryAttributes = (query, default_attr) => {
     return valid_attr.split(',').map(str => str.trim())
 }
 
+/**
 @param {Object}
 @returns {Array}
+*/
 
 export const getQueryOrder = query => {
     const { order_key = 'id', order_dir = 'ASC' } = query;
     return [[order_key, order_dir]];
 }
 
+/**
 @param {Object}
 @returns {Number}
+*/
 
 export const getQueryLimit = query => {
     const { limit } = query
