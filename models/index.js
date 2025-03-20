f8b74862fba6ba94cbed8613ebff630db80f634c
JustDondo <162633910+CodeDondo@users.noreply.github.com>
2025-03-20 13:03:19 +0100

    changesInIndex

--- models/index.js
+++ models/index.js
@@ -1,78 +1,52 @@
-import { userModel } from "./userModel.js";
-import { cityModel } from "./cityModel.js";
-import { estateModel } from "./estateModel.js";
-import { typeModel } from "./typeModel.js";
-import { imageModel } from "./imageModel.js";
-import { estateImageRelModel } from "./estateImageRelModel.js";
-import { energyLabelModel } from "./energyLabelModel.js";
-import { reviewModel } from "./reviewModel.js";
-import { staffModel } from "./staffModel.js";
-import { favouriteModel } from "./favouriteModel.js";
+import express from 'express'
+import cors from 'cors'
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dbController } from './Controllers/dbController.js'
+import { userController } from './controllers/userController.js'
+import { authController } from './controllers/authController.js'
+import { cityController } from './controllers/cityController.js'
+import { reviewController } from './controllers/reviewController.js'
+import { estateController } from './controllers/estateController.js'
+import { favoriteController } from './controllers/favoriteController.js'
+import { staffController } from './controllers/staffController.js'
 
-estateModel.belongsTo(cityModel, {
-    foreignKey: 'city_id'
+// Express Route Settings
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
+
+// Fix for __dirname in ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files from 'assets' folder (Change if needed)
+app.use('/images', express.static(path.join(__dirname, 'assets/images')));
+
+// Route for root
+app.get('/', (req, res) => {
+    res.send('Hello world')
 })
 
-cityModel.hastMany(estateModel, {
-    foreignKey: 'city_id'
+// Use controllers
+app.use(
+    estateController,
+    cityController,
+    reviewController,
+    favoriteController,
+    staffController,
+    userController,
+    authController,
+    dbController
+)
+
+// 404 route - skal være sidst!
+app.get('*', (req, res) => {
+    res.send('404 - kunne ikke finde siden')
 })
 
-estateModel.belongsTo(energyLabelModel, {
-    foreignKey: 'energy_label_id'
+// Server settings
+app.listen(4242, () => {
+    console.log(`Server kører på adressen http://localhost:4242`)
 })
-
-energyLabelModel.hasMany(estateModel, {
-    foreignKey: 'energy_label_id'
-})
-
-estateModel.belongsTo(typeModel, {
-    foreignKey: 'type_id'
-})
-
-typeModel.hasMany(estateModel, {
-    foreignKey: 'type_id'
-})
-
-reviewModel.beliongsTo(estateModel, {
-    foreignKey: 'estate_id'
-})
-
-estateModel.hasMany(reviewModel, {
-    foreignKey: 'estate_id'
-})
-
-reviewModel.belongsTo(userModel, {
-    foreignKey: 'user_id'
-})
-
-userModel.hasMany(reviewModel, {
-    foreignKey: 'user_id'
-})
-
-estateModel.belongsToMany(favouriteModel, {
-    through: favouriteModel,
-    foreignKey: 'estate_id'
-})
-
-userModel.belongsToMany(favouriteModel, {
-    through: favouriteModel,
-    foreignKey: 'user_id'
-})
-
-estateModel.belongsToMany(imageModel, {
-    through: estateImageRelModel,
-    as: 'images'
-})
-
-export {
-    userModel,
-    energyLabelModel,
-    estateModel,
-    typeModel,
-    imageModel,
-    estateImageRelModel,
-    reviewModel,
-    staffModel,
-    favouriteModel,
-    cityModel
-}
